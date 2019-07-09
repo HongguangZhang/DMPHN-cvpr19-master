a974aaad3a371e4aca2c9564d36f4cd7dff927e2
Hongguang Zhang <hongguang_zhang@Hongguang.Zhang>
2019-07-09 12:02:45 +1000

    initial version

--- models.py
+++ models.py
@@ -18,25 +18,25 @@ class Encoder(nn.Module):
             nn.Conv2d(32, 32, kernel_size=3, padding=1)
             )
         #Conv2
-        self.layer4 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
-        self.layer5 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1)
-            )
+        self.layer5 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
         self.layer6 = nn.Sequential(
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, padding=1)
             )
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1)
+            )
         #Conv3
-        self.layer7 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
-        self.layer8 = nn.Sequential(
+        self.layer9 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
+        self.layer10 = nn.Sequential(
             nn.Conv2d(128, 128, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv2d(128, 128, kernel_size=3, padding=1)
             )
-        self.layer9 = nn.Sequential(
+        self.layer11 = nn.Sequential(
             nn.Conv2d(128, 128, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv2d(128, 128, kernel_size=3, padding=1)
@@ -48,67 +48,66 @@ class Encoder(nn.Module):
         x = self.layer2(x) + x
         x = self.layer3(x) + x
         #Conv2
-        x = self.layer4(x)
-        x = self.layer5(x) + x
+        x = self.layer5(x)
         x = self.layer6(x) + x
+        x = self.layer7(x) + x
         #Conv3
-        x = self.layer7(x)    
-        x = self.layer8(x) + x
-        x = self.layer9(x) + x 
+        x = self.layer9(x)    
+        x = self.layer10(x) + x
+        x = self.layer11(x) + x 
         return x
 
 class Decoder(nn.Module):
     def __init__(self):
         super(Decoder, self).__init__()        
         # Deconv3
-        self.layer10 = nn.Sequential(
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1)
-            )
-        self.layer11 = nn.Sequential(
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1)
-            )
-        self.layer12 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)
-        #Deconv2
         self.layer13 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1)
+            nn.Conv2d(128, 128, kernel_size=3, padding=1)
             )
         self.layer14 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1)
+            )
+        self.layer16 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)
+        #Deconv2
+        self.layer17 = nn.Sequential(
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, padding=1)
             )
-        self.layer15 = nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1)
+        self.layer18 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1)
+            )
+        self.layer20 = nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1)
         #Deconv1
-        self.layer16 = nn.Sequential(
+        self.layer21 = nn.Sequential(
             nn.Conv2d(32, 32, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv2d(32, 32, kernel_size=3, padding=1)
             )
-        self.layer17 = nn.Sequential(
+        self.layer22 = nn.Sequential(
             nn.Conv2d(32, 32, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv2d(32, 32, kernel_size=3, padding=1)
             )
-        self.layer18 = nn.Conv2d(32, 3, kernel_size=3, padding=1)
+        self.layer24 = nn.Conv2d(32, 3, kernel_size=3, padding=1)
         
     def forward(self,x):        
         #Deconv3
-        x = self.layer10(x) + x
-        x = self.layer11(x) + x
-        x = self.layer12(x)                
-        #Deconv2
         x = self.layer13(x) + x
         x = self.layer14(x) + x
-        x = self.layer15(x)
-        #Deconv1
-        x = self.layer16(x) + x
+        x = self.layer16(x)                
+        #Deconv2
         x = self.layer17(x) + x
-        x = self.layer18(x)
+        x = self.layer18(x) + x
+        x = self.layer20(x)
+        #Deconv1
+        x = self.layer21(x) + x
+        x = self.layer22(x) + x
+        x = self.layer24(x)
         return x
-
